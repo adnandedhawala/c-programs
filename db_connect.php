1d39ddb4e475450d671a516c6e591570b157a92b
LAPTOP-S3AKPKRL\lenovo <adnan.d@somaiya.edu>
2018-03-06 07:02:23 +0530

    pls omit

--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php  
-	if(session_id()=="")session_start();
-
-	$conn=mysqli_connect("localhost","root","","cinema");
-
-	// print_r($conn);
-?>
--- login.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	require_once 'header.php';
-?>
-
-<div class="content"></div>
-
-<?php  
-	require_once 'footer.php';
-?>
